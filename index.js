523f99e2d51e1179fffe1c7678620d825f6f7e64
Vladimir S <shalamanov@gmail.com>
2017-05-09 23:35:32 +0300

    Added configurable environment property for http port

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ http.createServer(function (request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"}); 
 	response.write("Hello World");
 	response.end();
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
